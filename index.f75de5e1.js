a424411317af4aaabffd4de8605cb99f5be1ad4b
Teneta Evhen <tenetaevgen@gmail.com>
2024-07-08 21:59:30 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,22 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const notification = document.createElement("div");
+    const notificationTitle = document.createElement("h2");
+    const notificationDescription = document.createElement("p");
+    notificationTitle.textContent = title;
+    notificationDescription.textContent = description;
+    notification.appendChild(notificationTitle);
+    notification.appendChild(notificationDescription);
+    notification.classList.add("notification", type);
+    notification.style.top = `${posTop}px`;
+    notification.style.right = `${posRight}px`;
+    document.body.appendChild(notification);
+    setTimeout(()=>{
+        notification.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(170, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
